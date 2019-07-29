1e1df37cab624189964e4cd0508c4f8b1e7a47b7
prabaharanh <prabaharan.haridass@outlook.com>
2019-07-30 02:13:47 +1200

    port

--- dbConfig.php
+++ dbConfig.php
@@ -4,6 +4,6 @@ define ("DB_USER", "root");
 define ("DB_PASS",""); 
 define ("DB_NAME","dbeta"); 
 
-$link = mysqli_connect(DB_HOST, DB_USER, DB_PASS) or die("Couldn't make connection.");
+$link = mysqli_connect(DB_HOST:50062, DB_USER, DB_PASS) or die("Couldn't make connection.");
 $db = mysqli_select_db($link, DB_NAME) or die("Couldn't select database");
 ?>
