24be5ecad0b64fc6e531d54a071dc2f4eccd7116
prabaharanh <prabaharan.haridass@outlook.com>
2019-07-30 02:05:52 +1200

    db services

--- dbConfig.php
+++ dbConfig.php
@@ -1,6 +1,6 @@
 <?php
-define ("DB_HOST", "etasolution.scm.azurewebsites.net"); 
-define ("DB_USER", "azure"); 
+define ("DB_HOST", "localhost"); 
+define ("DB_USER", "root"); 
 define ("DB_PASS",""); 
 define ("DB_NAME","dbeta"); 
 
