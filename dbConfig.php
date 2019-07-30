c5e0ae0292ff4f68ebb97afdc5a48143c4f1bf3a
prabaharanh <prabaharan.haridass@outlook.com>
2019-07-30 13:33:30 +1200

    change1

--- dbConfig.php
+++ dbConfig.php
@@ -4,6 +4,6 @@ define ("DB_USER", "eta@etasolutions");
 define ("DB_PASS","Radhah@5969"); 
 define ("DB_NAME","dbeta"); 
 
-$link = mysqli_connect(DB_HOST, DB_USER, DB_PASS) or die("Couldn't make a  connection.");
+$link = mysqli_connect(DB_HOST, DB_USER, DB_PASS) or die("Couldn't make a test  connection.");
 $db = mysqli_select_db($link, DB_NAME) or die("Couldn't select database");
 ?>
