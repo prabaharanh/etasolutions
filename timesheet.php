7a5f894c89b21189cf61d57b517ab76aa2ffecda
prabaharanh <prabaharan.haridass@outlook.com>
2019-10-03 13:03:44 +1300

    devops built commit

--- timesheet.php
+++ timesheet.php
@@ -30,7 +30,7 @@ $pageLimit = ($page * $setLimit) - $setLimit;
           <div class="modal-dialog" role="document">
             <div class="modal-content">
               <div class="modal-header">
-                <h5 class="modal-title" id="exampleModalLabel">Add Timesheet </h5>
+                <h5 class="modal-title" id="exampleModalLabel">Add Timesheet for employee </h5>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                   <span aria-hidden="true">&times;</span>
                 </button>
